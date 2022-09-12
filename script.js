1ef64dca17662720a30dbc6ee6b23683cb3846f9
BuildTools <unconfigured@null.spigotmc.org>
2022-09-12 18:02:16 +0200

    modified:   script.js

--- script.js
+++ script.js
@@ -187,12 +187,13 @@ TEXT_appMenu_SPOTIFY.addEventListener("focusout", async function(e){
 
 //= Deezer integration =
 async function getJsonPlaylistDeezer(playlistID, offset) {
-    let response = await fetch('https://cors-anywhere.herokuapp.com/https://api.deezer.com/playlist/'+playlistID+ "/tracks?index="+offset+"&limit="+trackLimit, {
-        method: 'GET',
-        'Access-Control-Allow-Origin': '*',
-        'Accept': 'application/json',
+    let response = await fetch('https://api.deezer.com/playlist/'+playlistID+ "/tracks?index="+offset+"&limit="+trackLimit+"&request_method=GET", {
+        'Access-Control-Allow-Credentials' : true,
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Methods':'GET',
+        'Access-Control-Allow-Headers':'application/json',
     })
-    let data = await response.json()
+    let data = await response.json();
     return data;
 }
 
